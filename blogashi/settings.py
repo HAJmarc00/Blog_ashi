23556da71e8e7654344ea2145aa2c1855e9e1d02
Donald Trump <hajaceu@gmail.com>
2025-06-10 19:18:15 +0330

    add concentual modeling for article and category and userprofile

--- blogashi/settings.py
+++ blogashi/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'blogs',  
+    'rest_framework',  
+    'ckeditor', 
 ]
 
 MIDDLEWARE = [
@@ -74,8 +80,12 @@ WSGI_APPLICATION = 'blogashi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',    
+        'PORT': '5432',
     }
 }
 
@@ -114,9 +124,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CKEditor settings
+# Static files 
+STATIC_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = 'static/'
+
+# CKEditor 
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
+CKEDITOR_UPLOAD_PATH = "uploads/"
--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserProfile, Article, Category
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Article)
+admin.site.register(Category)
